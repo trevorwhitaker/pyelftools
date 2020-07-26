7b9dc631fd799f4b8c00af0c907b7fcda141b564
Whitaker <twhitake@hotelppcle03.torolab.ibm.com>
2020-07-26 18:55:05 -0400

    First drop of ppc support

--- elftools/dwarf/descriptions.py
+++ elftools/dwarf/descriptions.py
@@ -161,6 +161,8 @@ def describe_reg_name(regnum, machine_arch=None, default=True):
         return _REG_NAMES_x64[regnum]
     elif machine_arch == 'AArch64':
         return _REG_NAMES_AArch64[regnum]
+    elif machine_arch == '64-bit PowerPC':
+        return _REG_NAMES_PPC64[regnum]
     elif default:
         return 'r%s' % regnum
     else:
@@ -538,6 +540,16 @@ _REG_NAMES_AArch64 = [
     'x30', 'sp'
 ]
 
+# https://refspecs.linuxfoundation.org/ELF/ppc64/PPC-elf64abi.html#RELOC-TYPE
+_REG_NAMES_PPC64 = [
+    'r0', 'r1', 'r2', 'r3', 'r4', 'r5', 'r6', 'r7', 'r8', 'r9',
+    'r10', 'r11', 'r12', 'r13', 'r14', 'r15', 'r16', 'r17', 'r18', 'r19',
+    'r20', 'r21', 'r22', 'r23', 'r24', 'r25', 'r26', 'r27', 'r28', 'r29',
+    'r30', 'r31', 'f0', 'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7',
+    'f8', 'f9', 'f10', 'f11', 'f12', 'f13', 'f14', 'f15', 'f16', 'f17',
+    'f18', 'f19', 'f20', 'f21', 'f22', 'f23', 'f24', 'f25', 'f26', 'f27',
+    'f28', 'f29', 'f30', 'f31'
+]
 
 class ExprDumper(object):
     """ A dumper for DWARF expressions that dumps a textual
